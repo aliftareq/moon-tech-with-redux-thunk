6b765fc971dc0743cfe6025b25336c641bfef4cb
aliftareq <aliftareq@gmail.com>
2023-03-24 00:01:17 +0600

    first commit

--- src/redux/actionTypes/actionTypes.js
+++ src/redux/actionTypes/actionTypes.js
@@ -1,5 +1,12 @@
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const PRODUCT_LOADED = "PRODUCT_LOADED";
+
+//for add&remove product
 export const ADD_PRODUCT = "ADD_PRODUCT";
 export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
+export const LOAD_PRODUCT = 'LOAD_PRODUCT'
+
+//for filter 
+export const TOGGLE_BRAND = 'TOGGLE_BRAND';
+export const TOGGLE_STOCK = 'TOGGLE_STOCK';
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
-import productReducer from "./reducers/productReducer";
+import rootReducer from "./reducers/rootReducer";
+import logger from "redux-logger";
+import cartCounter from "./middlewares/cartCounter";
+import thunk from "redux-thunk";
 
-const store = createStore(productReducer, composeWithDevTools());
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(cartCounter, thunk, logger)));
 
 export default store;
